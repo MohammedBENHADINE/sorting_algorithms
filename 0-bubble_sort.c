85c7ad6624beda7f6ba6edebe7fc738a0a59b4f8
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-29 16:45:38 -0700

    add big Oh rating

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
+	if (size < 2)
+		return;
 	for (i = 0; i < size; i++)
 		for (j = 0; j < size - 1 - i; j++)
 			if (array[j + 1] < array[j])
